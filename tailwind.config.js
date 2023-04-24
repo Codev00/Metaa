8f1b443f661c5696da69e4158a0d0c350595c15d
Codev00 <kenngok2@gmail.com>
2023-04-24 20:13:49 +0700

    delete

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-   purge: ["./public/**/*.html", "./src/**/*.{html,js,jsx}"],
-   content: ["./src/**/*.{html,js,jsx}"],
-   theme: {
-      extend: {},
-   },
-   plugins: [],
-};
